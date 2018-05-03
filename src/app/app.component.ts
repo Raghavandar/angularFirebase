73c5862319738b859b656f2366d3973bd8aadfc7
raghavandar <raghavandar@gmail.com>
2018-05-03 18:06:21 +0530

    03-05-2018 EOD commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { AngularFireDatabase, AngularFireObject, AngularFireList } from 'angularfire2/database';
+import { Subscription } from 'rxjs/Subscription';
+import { Observable } from '@firebase/util';
+import { query } from '@angular/core/src/animation/dsl';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnDestroy {
+
+  courses$;
+  courses1$: AngularFireList<{}>;
+  course$;
+  author$;
+  //courses: any[];
+  //subscription: Subscription;
+
+  constructor(private dp: AngularFireDatabase) {
+    this.courses$ = dp.list('/courses').valueChanges();
+    this.courses1$ = dp.list('/courses');
+    this.course$ = dp.object('/courses/1').valueChanges();
+    this.author$ = dp.object('/authors/1').valueChanges();
+
+    console.log(this.courses1$);
+    /* this.subscription = dp.list('/courses').valueChanges().subscribe(courses => {
+      this.courses = courses;
+      console.log(this.courses);
+    }); */
+  }
+
+  add(courseName: HTMLInputElement) {
+    this.courses1$.push({'name': courseName.value, 'price': 1200, 'sections':['dwfq', 'dwa']});
+    courseName.value = '';
+  }
+
+  update(course) {
+    this.dp.object('/courses/1').set(course + 'updated');
+  }
+
+  delete() {
+
+  }
+
+  ngOnDestroy() {
+    //this.subscription.unsubscribe();
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAwnzoXPP-YKYx89Z124fyZvdCh-bHhgak",
+    authDomain: "fir-demo-3d3fd.firebaseapp.com",
+    databaseURL: "https://fir-demo-3d3fd.firebaseio.com",
+    projectId: "fir-demo-3d3fd",
+    storageBucket: "fir-demo-3d3fd.appspot.com",
+    messagingSenderId: "175628515708"
+  }
 };
